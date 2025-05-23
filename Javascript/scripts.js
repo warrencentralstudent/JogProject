fcb2f8dea79ec803241644f415ff544661ecf5e4
Davian Affognon <affogdav000@warren.k12.in.us>
2025-05-23 08:46:23 -0400

    yeah

---
+++ Javascript/scripts.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    document.getElementById("myForm").submit();
+}
+
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    window.alert('Form submitted!');
+});
+
+function openJob(evt, tabname) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
